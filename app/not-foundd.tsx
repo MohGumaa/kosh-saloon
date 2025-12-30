bb456c9d6eb3aca6aacbf03cfab5e9707672945c
MohGuma <gomaam14@gamil.com>
2025-12-31 03:42:38 +0400

    add mode toggle theme

---
+++ app/not-foundd.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      not-found.tsx
+    </div>
+  )
+}
+
+export default NotFoundPage
---
+++ fonts/index.ts
@@ -0,0 +1,7 @@
+import { Tajawal, Roboto } from 'next/font/google';
+
+export const tajawal = Tajawal({
+	weight: ['400', '500', '700'],
+	subsets: ['arabic'], // For Arabic language support
+	display: 'swap',
+});
