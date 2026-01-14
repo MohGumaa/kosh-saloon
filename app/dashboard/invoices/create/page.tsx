4d2afac01a8d67238e31f8bbc5795c4af6d8d6b5
MohGuma <gomaam14@gamil.com>
2026-01-14 13:54:40 +0400

    update the layout for create invoice page

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,8 +1,25 @@
-const CreateInvoicesPage = () => {
+import Breadcrumbs from "@/components/invoices/breadcrumbs"
+import Form from "@/components/invoices/create-form";
+import { fetchServices, fetchStaffs } from "@/lib/data";
+
+const CreateInvoicesPage = async() => {
+  const staffs = await fetchStaffs();
+  const services = await fetchServices();
+
   return (
-    <div>
-      create invoices
-    </div>
+   <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'الفواتير', href: '/dashboard/invoices' },
+          {
+            label: 'إنشاء فاتورة',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form staffs={staffs} services={services}/>
+    </main>
   )
 }
 
